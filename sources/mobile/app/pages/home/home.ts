078b92cb0bcbd97ec11413303937bd34a5be3064
JP Durot <jeanphilippe.durot@gmail.com>
2016-01-19 00:27:14 +0100

    Added info on bluetooth connection

--- sources/mobile/app/pages/home/home.ts
+++ sources/mobile/app/pages/home/home.ts
@@ -1,14 +1,33 @@
-import {Page, NavController} from 'ionic-framework/ionic';
+import {Page, NavController, Alert} from 'ionic-framework/ionic';
 
 import {SettingsPage} from '../settings/settings';
+
+import {BluetoothService, IBluetoothStatus, IBluetoothDevice} from '../../services/bluetoothService';
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
     private nav: NavController;
+    private bluetoothService: BluetoothService;
+    
+    private bluetoothInfo: IBluetoothStatus;
+    
   constructor(nav: NavController) {
     this.nav = nav;
+    this.bluetoothService = new BluetoothService();
+    this.bluetoothInfo = this.bluetoothService.getStatus();
+    this.bluetoothService.connect("CatFeeder").then(
+        (result) => {},
+        (error)=>
+        {
+            let alert = Alert.create({
+                title: 'Bluetooth',
+                subTitle: 'Unable to connect',
+                buttons: ['Dismiss']
+            });
+            this.nav.present(alert);
+        }
   }
   
   openSettings()
