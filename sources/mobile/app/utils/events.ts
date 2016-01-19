de28c9a5b6062927ca9608b31a0177c839fdafed
JP Durot <jeanphilippe.durot@gmail.com>
2016-01-19 23:36:50 +0100

    Simple event handling system

---
+++ sources/mobile/app/utils/events.ts
@@ -0,0 +1,20 @@
+interface ICustomEvent<T> {
+    on(handler: { (data?: T): void });
+    off(handler: { (data?: T): void });
+}
+
+class CustomEvent<T> implements ICustomEvent<T> {
+    private handlers: { (data?: T): void; }[] = [];
+
+    public on(handler: { (data?: T): void }) {
+        this.handlers.push(handler);
+    }
+
+    public off(handler: { (data?: T): void }) {
+        this.handlers = this.handlers.filter(h => h !== handler);
+    }
+
+    public trigger(data?: T) {
+        this.handlers.slice(0).forEach(h => h(data));
+    }
+}
